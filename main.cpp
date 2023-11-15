44fd3990967274b349d15bc2227b5b832ad24e27
aidsvm <77755170+aidsvm@users.noreply.github.com>
2023-11-15 13:05:18 -0500

    Co-authored-by: Fabian Rosario <FabiBaby@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 #include <queue>
+#include <string>
+#include <vector>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 
 int main() {
     ifstream infile;
+    //This is a priority queue with a mean heap implementation
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-    infile.open()
+
+    int i = 1;
+    while(i <= 2){
+        string filename = "input" + to_string(i);
+        infile.open(filename);
+
+    
+        string line;
+        int n, m, m_count;
+        vector<long int> arr;
+
+        int lineNum = 0;
+        while(getline(infile, line)) {
+            istringstream iss(line);
+            if(lineNum == 0){
+                iss >> n >> m;
+                cout << "Read numbers: " << n << " " << m << endl;
+            }
+    
+            else{
+                while(iss >> m_count) {
+                    arr.push_back(m_count);
+                }
+
+                for(int j = 0; j < m; j++) {
+                    cout << arr[j] << " ";
+                }
+            }
+            lineNum++;
+        }
+
+
+
+
+
+
+
+        i++;
+        }
+
+    infile.close();
 }
