4606eac2eb45bba27a52730ee4058cd0edae4573
Fabian Rosario <fabianraul1210@gmail.com>
2023-11-16 09:46:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ using namespace std;
 
 int main() {
     ifstream infile;
-    //This is a priority queue with a mean heap implementation
+    //This is a priority queue with a min heap implementation
     priority_queue<int, vector<int>, greater<int>> pq;
 
 
+    //While loops runs twice, once for each input
     int i = 1;
     while(i <= 2){
         string filename = "inputs/input" + to_string(i);
@@ -21,7 +22,7 @@ int main() {
     
         string line;
         int n, m, m_count;
-        vector<long int> arr;
+        vector<long int> times;
 
         int lineNum = 0;
         while(getline(infile, line)) {
@@ -32,14 +33,16 @@ int main() {
             }
     
             else{
+                //Adds all the times into the vector
                 while(iss >> m_count) {
-                    arr.push_back(m_count);
+                    times.push_back(m_count);
                 }
-
+                //For loop just comfirms that the values are in the vector
                 for(int j = 0; j < m; j++) {
-                    cout << arr[j] << " ";
+                    cout << times[j] << " ";
                 }
             }
+            
             lineNum++;
         }
 
